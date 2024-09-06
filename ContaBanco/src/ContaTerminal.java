2acb28af9052e5cb75b54299641246e6c44b3bd1
agnerage <marlon.agner@gmail.com>
2024-09-06 20:17:21 -0300

    Projeto de entrada com Scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+//Aplica o import do Scanner
+
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -10,6 +12,8 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in);
         
+        //Pede a entrada dos dados ao usuário
+
         System.out.println("Por favor, digite o número da Conta !");
             Numero = scanner.nextInt();
             scanner.nextLine(); 
@@ -23,9 +27,10 @@ public class ContaTerminal {
             System.out.println("Por favor, digite o número do saldo !");
             Saldo = scanner.nextDouble();
 
-            // Construir a mensagem final 
+            // Constroi a mensagem final sobre o status da conta
+
             System.out.println("Olá ".concat(Cliente).concat(", ")
-                .concat("obrigado por criar uma conta em nosso banco, sua agência é ")
+                .concat(" obrigado por criar uma conta em nosso banco, sua agência é ")
                 .concat(Agencia).concat(", ")
                 .concat("conta ").concat(String.valueOf(Numero)).concat(" e seu saldo ")
                 .concat(String.valueOf(Saldo)).concat(" já está disponível para saque."));
